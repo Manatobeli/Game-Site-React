940fddb277e0662a3da6d01fd06a0040e5c6933c
Manatobeli <george.khatchapuridze.1@btu.edu.ge>
2025-02-05 19:10:40 +0400

    changed api.js and hero-bg

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ const API_KEY = '8420ae4ca54e44e983cfc17d80ad5d7c';
 
 export const fetchGames = async (genre = '', search = '') => {
   try {
-    const url = `/api?key=${API_KEY}${genre ? `&genres=${genre}` : ''}${search ? `&search=${search}` : ''}&page_size=21`;
+    const baseUrl = 'https://api.rawg.io/api/games';
+    const url = `${baseUrl}?key=${API_KEY}${genre ? `&genres=${genre}` : ''}${search ? `&search=${search}` : ''}&page_size=21`;
 
     const response = await fetch(url);
     const data = await response.json();
